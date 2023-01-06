49c4bf2c006eedaf31d1c87b5e8c7505979fffb5
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 23:35:44 +0500

    count_jobs

--- count_jobs.py
+++ count_jobs.py
@@ -8,4 +8,9 @@ def count_jobs(data:list, job:str) -> int:
     Returns:
         int: The number of users with the given job
     """
-    return 
+    k=0
+    for i in data:
+        if i['job']==job:
+            k+=1
+    return k
+print(count_jobs([{'name': 'John','job': 'Developer'},{'name': 'Mary','job': 'Developer'}],'Developer'))
