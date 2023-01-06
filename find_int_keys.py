1926f12e0cac0abdd9474310d0a3b01555c21eec
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 16:16:24 +0500

    find_int_keys

--- find_int_keys.py
+++ find_int_keys.py
@@ -9,8 +9,8 @@ def find_int_keys(data: dict) -> list:
     keys=data.keys()
     list1=[]
     for i in keys:
-        if i%1==0:
+        if type(i)==int:
             list1.append(i)
     return list1
-print(find_int_keys({1:'davron',2:'asd',3.3:'gfdsa'}))
+print(find_int_keys({1:'davron',2:'asd','a':'gfdsa'}))
  
