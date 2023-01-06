da7e5d65cde1f00f49e0eadde445604e7e6c3e68
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 22:58:23 +0500

    get_user_names_with_age_range

--- get_user_names_with_age_range.py
+++ get_user_names_with_age_range.py
@@ -9,4 +9,9 @@ def get_user_names_with_age_range(data:list, min_age:int, max_age:int) -> list:
     Returns:
         list: A list of users with the given age range
     """
-    return 
+    list1=[]
+    for i in data:
+        if i['age']>min_age and i['age']<max_age:
+            list1.append(i['name'])
+    return list1
+print(get_user_names_with_age_range([{'name': 'John','age': 20},{ 'name': 'Mary','age': 17},{'name': 'Ban','age': 23}],20,25)) 
