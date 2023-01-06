d7bbad31da2c7d2168a189af10fc80449f77682e
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 16:28:16 +0500

    find_length_of_values

--- find_length_of_values.py
+++ find_length_of_values.py
@@ -6,4 +6,11 @@ def find_length_of_values(data: dict) -> int:
     Returns:
         int: The sum of the length of all values in the dictionary.
     """
-    return 
+    values=data.values()
+    list1=list(values)
+    sum=0
+    for i in list1:
+        sum+=len(i)
+
+    return sum
+print(find_length_of_values({1:'fdsa',2:'wertgfd'})) 
