a36cc2c2e0a2f652e390d5ccb5d94204502e79d5
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 16:40:17 +0500

    find_max_value

--- find_max_value.py
+++ find_max_value.py
@@ -6,4 +6,11 @@ def find_max_value(data: dict):
     Returns:
         int: The maximum value in the dictionary.
     """
-    return 
+    values=data.values()
+    list1=list(values)
+    max=0
+    for i in list1:
+        if max<i:
+            max=i
+    return max
+print(find_max_value({1:25, 9.1:40, 30:60.5}))
