78c3d4c2dbcd55529d6413066c6c4b7bf32a126b
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2023-01-06 23:06:17 +0500

    get_user_names_with_age

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,4 +8,7 @@ def get_user_names_with_age(data:list, age:int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    return 
+    for i in data:
+        if i['age']==age:
+            return i['name'] 
+print(get_user_names_with_age([{'name': 'John','age': 27},{'name': 'Mary', 'age': 42}],42))
